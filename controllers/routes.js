f4fe1b68031d54ae705b97443aa6fed320e6ac3c
Ajayraj1113 <ajayraj824@gmail.com>
2025-06-05 13:44:22 +0530

    showEmp and DeleteEmp functionality added

---
+++ controllers/routes.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const router = express.Router()
+const Employee = require("../models/connection.model")
+
+
+// router.get("/", (req, res)=> {
+//     res.render("home");
+// });
+
+router.get("/add-emp", (req, res) => {
+    res.render("addEmp")
+})
+
+router.post("/save-emp", async (req,res)=>{
+    // console.log(req.body)
+    try {
+        const Emp = new Employee({
+            fullname : req.body.fullname,
+            email : req.body.email,
+            phone : req.body.mobile,
+            city : req.body.city
+        })
+
+       await Emp.save()
+        res.redirect("/")
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+// show data
+router.get("/show-all-emp", async (req,res)=>{
+    try {
+        const result = await Employee.find()
+        // console.log(result)
+        res.render("showEmp", {list : result})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get("/delete-all-emp", async (req, res)=>{
+    try {
+        const result = await Employee.find()
+        // console.log(result)
+        res.render("deleteEmp", {list : result})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get("/final-delete/:uid", async (req, res)=>{
+    try {
+        const result = await Employee.findByIdAndDelete(req.params.uid)
+        console.log(result)
+        res.redirect("/emp/delete-all-emp")
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require("express")
 const connectDB = require("./models/db")
-const app = express()
 const engine = require("express-handlebars").engine
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
-connectDB()
+const app = express();
+// const route = require("./controllers/routes")
 
-const PORT = 3000
+connectDB();
 
-app.use(express.json()) // accepts json data
 
-app.engine('handlebars', engine());
+app.use(express.json()); // accepts json data
+app.use(express.urlencoded({extended : true}));
+
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    // ...implement newly added insecure prototype access
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+    })
+);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('home');
+
+app.get("/", (req, res)=> {
+    res.render("home");
 });
 
-// app.use("/Emp", )
+app.use('/emp', require('./controllers/routes'));
+// app.use('/admin', require('./controllers/admin'));
 
+const PORT = process.env.PORT || 5050
 app.listen(PORT, ()=> {
     console.log(`Server is running on port http://localhost/${PORT}`)
 })
